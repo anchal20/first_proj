bb9892a4d4fa288f0431c95bdefc423cffd6f2c6
anchal20 <anchalsachdeva20@gmail.com>
2014-07-30 23:09:49 +0530

    My first commit

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+/******************document ready function*******************/
+$(document).ready(function(){
+   $(".nav-bar-div").click(function(){
+      $(this).hide(); 
+   });
+    
+/*****    var name_div= $(".name-div");
+    name_div.animate({marginLeft: '390px'});
+    
+    name_div.animate({fontSize: '150px'}, "slow"); ****/
+    
+});
