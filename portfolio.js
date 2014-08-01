3bbbe689f2da0e486558174e1a45491483788cc9
anchal20 <anchalsachdeva20@gmail.com>
2014-08-01 20:17:00 +0530

    70% complete 1/8/2014

--- portfolio.js
+++ portfolio.js
@@ -54,9 +54,56 @@ window.onload=function() {
         setTimeout(function(){
             $("#home-section").removeClass("reset-intro-content");
             $("#home-section").addClass("animate-intro");
-             
+            $("#H").css({"display":"inline-block"});
+            $("#I").css({"display":"inline-block"});
+            $("#blank").css({"display":"block"});
+            $("#exclm").css({"display":"inline-block"});
+            
             $(".big-heading").children().css("background-position","0px -10px");
             },100);
         
      HomeHeadingAnimation();    
 };
+
+/********************************* cube & skill integration ****************************/
+function stoprotate() {
+    $(".cube").addClass("stopRotation");
+}
+
+function resumerotate() {
+    $(".cube").removeClass("stopRotation");
+}
+
+function skillstart(id) {
+    var skill_id = id;
+    if (skill_id == "div1"){
+    $("#div1").addClass("skill1-animate");
+    }
+    else if (skill_id == "div2") {
+    $("#div2").addClass("skill2-animate");
+    }
+    else if (skill_id == "div3") {
+    $("#div3").addClass("skill3-animate");
+    }
+    else {
+    $("#div4").addClass("skill4-animate");
+    }
+}
+
+function skillstop(id) {
+    var skill_id = id;
+    if (skill_id == "div1"){
+    $("#div1").removeClass("skill1-animate");
+    }
+    else if (skill_id == "div2") {
+    $("#div2").removeClass("skill2-animate");
+    }
+    else if (skill_id == "div3") {
+    $("#div3").removeClass("skill3-animate");
+    }
+    else {
+    $("#div4").removeClass("skill4-animate");
+    }
+}
+
+/****************************************************************************************/
