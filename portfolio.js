e4edb1286d7b83b79172cfa8c31bfa36059814a0
anchal20 <anchalsachdeva20@gmail.com>
2014-08-10 21:56:37 +0530

    new home page n style

--- portfolio.js
+++ portfolio.js
@@ -1,26 +1,31 @@
 /******************document ready function*******************/
 $(document).ready(function(){   
-    $(".big-heading").children().css("background-position","0px 135px");
-    $("#home-section").addClass("reset-intro-content");
+    
+    
     $("#contactID").hide();
     
-    $("#plane").sprite({fps: 8, no_of_frames: 14});
-    
+    $("#plane").sprite({fps: 4, no_of_frames: 14});
+    $('#sun').sprite({fps:4, no_of_frames: 4});
     
 });
 /********* ends here **********/
-/****function moveBgAround() {
+function select_screen(screen_id) {
+    var id = screen_id;
     
-    var x = Math.floor(Math.random()*401);
-    var y = Math.floor(Math.random()*401);
-    
-    var time = Math.floor(Math.random()*1001) + 2000;
-    
-    $(".scroll-name").animate({
-        backgroundPosition: '(' + (x * -1) + 'px ' + (y * -1) + 'px)'
-    }, time, "swing", function() {
-        moveBgAround();
-    }); ***/
+    if (id=='skill_ref'){
+        $("#skill_section").removeClass("hide inactive").addClass("active show");
+        $("#home_section").removeClass("active show").addClass("hide inactive");
+        $("#contact_section").removeClass("active show").addClass("hide inactive");
+        $("#about-me").removeClass("active show").addClass("hide inactive");
+    }
+    else if (id=='about') {
+        $("#skill_section").removeClass("show active").addClass("inactive hide");
+        $("#home_section").removeClass("active show").addClass("hide inactive");
+        $("#contact_section").removeClass("active show").addClass("hide inactive");
+        $("#about-me").removeClass("inactive hide").addClass("show active");
+    }
+        
+}
 /*********************** heading animation **************************************/
 function animateHeadingText(){
     var rannum1=Math.floor(Math.random()*15+1);
@@ -32,7 +37,7 @@ function animateHeadingText(){
     var selector3  = '.big-heading span:nth-child(' + rannum3 + ')';
     
     $(selector1).css("background-position","0px -168px").delay(80);
-    $(selector1).css("background-position","0px -10px");
+    $(selector1).   css("background-position","0px -10px");
     $(selector2).css("background-position","0px -168px").delay(80);
     $(selector2).css("background-position","0px -168px");
     $(selector3).css("background-position","0px -168px").delay(80);
@@ -46,61 +51,13 @@ function HomeHeadingAnimation(){
 }
 /********************* onload function ***************************/
 window.onload=function() {
-  $("#home-section").removeClass("hide inactive").addClass("active show");
-        setTimeout(function(){
-            $("#home-section").removeClass("reset-intro-content");
-            $("#home-section").addClass("animate-intro");
-            $("#H").css({"display":"inline-block"});
-            $("#I").css({"display":"inline-block"});
-            $("#blank").css({"display":"block"});
-            $("#exclm").css({"display":"inline-block"});
-            
-            $(".big-heading").children().css("background-position","0px -10px");
-            },100);
-        
-     HomeHeadingAnimation();    
+    $("#main").addClass("black");
+    $("#skill_section").removeClass("active show").addClass("inactive hide");
+    $("#home_section").removeClass("inactive hide").addClass("show active");
+    $("#about-me").removeClass("active show").addClass("hide inactive");
 };
 
-/********************************* cube & skill integration ****************************/
-function stoprotate() {
-    $(".cube").addClass("stopRotation");
-}
 
-function resumerotate() {
-    $(".cube").removeClass("stopRotation");
-}
-
-function skillstart(id) {
-    var skill_id = id;
-    if (skill_id == "div1"){
-    $("#div1").addClass("skill1-animate");
-    }
-    else if (skill_id == "div2") {
-    $("#div2").addClass("skill2-animate");
-    }
-    else if (skill_id == "div3") {
-    $("#div3").addClass("skill3-animate");
-    }
-    else {
-    $("#div4").addClass("skill4-animate");
-    }
-}
-
-function skillstop(id) {
-    var skill_id = id;
-    if (skill_id == "div1"){
-    $("#div1").removeClass("skill1-animate");
-    }
-    else if (skill_id == "div2") {
-    $("#div2").removeClass("skill2-animate");
-    }
-    else if (skill_id == "div3") {
-    $("#div3").removeClass("skill3-animate");
-    }
-    else {
-    $("#div4").removeClass("skill4-animate");
-    }
-}
 
 /****************************************************************************************/
 
