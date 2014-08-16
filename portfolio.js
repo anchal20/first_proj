ce7efc9a23b9cd6455be1f781af11df8b8c72a6c
anchal20 <anchalsachdeva20@gmail.com>
2014-08-16 19:32:17 +0530

    almost final

--- portfolio.js
+++ portfolio.js
@@ -20,7 +20,7 @@ function select_screen(screen_id) {
         $("#about-me").removeClass("active show").addClass("hide inactive");
     }
     else if (id=='about') {
-        $('#main_body').removeClass("black white").addClass("blue bubbles");
+        $('#main_body').removeClass("black white").addClass("blue");
         $("#skill_section").removeClass("show active").addClass("inactive hide");
         $("#home_section").removeClass("active show").addClass("hide inactive");
         $("#contact_section").removeClass("active show").addClass("hide inactive");
@@ -63,7 +63,8 @@ window.onload=function() {
 
 /****************************************************************************************/
 
-/***************************** contacts **************************************/
+
+/*********************************** contacts ********************************************/
 function closeContact() {
     $('#infoID').slideDown();
     $('#contactID').slideUp();
@@ -73,4 +74,55 @@ function showContact() {
     $('#infoID').hide();
     $('#contactID').slideDown();   
 }
-/*******************************************************************************/
+/***************************************************************************************/
+
+/******************************skill display functions*************************************/
+function html_disp() {
+    $('#direct').addClass("html_mov").removeClass("css_mov java_mov jquery_mov");
+    $('#h-board').removeClass("hide inactive").addClass("show active");
+    $('#c-board').removeClass("show active").addClass("hide inactive");
+    $('#j-board').removeClass("show active").addClass("hide inactive");
+    $('#jq-board').removeClass("show active").addClass("hide inactive");
+    $('#rd1').removeClass("hide inactive").addClass("show active");
+    $('#rd').removeClass("show active").addClass("hide inactive");
+}
+
+function css_disp() {
+    $('#direct').addClass("css_mov").removeClass("html_mov java_mov jquery_mov");
+    $('#c-board').removeClass("hide inactive").addClass("show active");
+    $('#rd').removeClass("hide inactive").addClass("show active");
+    $('#j-board').removeClass("show active").addClass("hide inactive");
+    $('#h-board').removeClass("show active").addClass("hide inactive");
+    $('#jq-board').removeClass("show active").addClass("hide inactive");
+    $('#rd1').removeClass("show active").addClass("hide inactive");
+}
+
+function java_disp() {
+    $('#direct').addClass("java_mov").removeClass("html_mov css_mov jquery_mov");
+    $('#j-board').removeClass("hide inactive").addClass("show active");
+    $('#c-board').removeClass("show active").addClass("hide inactive");
+    $('#rd1').removeClass("hide inactive").addClass("show active");
+    $('#h-board').removeClass("show active").addClass("hide inactive");
+    $('#jq-board').removeClass("show active").addClass("hide inactive");
+    $('#rd').removeClass("show active").addClass("hide inactive");
+}
+
+function jquery_disp() {
+    $('#direct').addClass("jquery_mov").removeClass("html_mov css_mov java_mov");
+    $('#rd').removeClass("hide inactive").addClass("show active");
+    $('#jq-board').removeClass("hide inactive").addClass("show active");
+    $('#c-board').removeClass("show active").addClass("hide inactive");
+    $('#h-board').removeClass("show active").addClass("hide inactive");
+    $('#j-board').removeClass("show active").addClass("hide inactive");
+    $('#rd1').removeClass("show active").addClass("hide inactive");
+}
+/****************************************************************************************/
+
+/************************************** about me ****************************************/
+function show_back() {
+    $('#com1').removeClass("hide inactive").addClass("show active");
+}
+function remove_com() {
+    $('#com1').removeClass("show active").addClass("hide inactive");
+}
+/****************************************************************************************/
